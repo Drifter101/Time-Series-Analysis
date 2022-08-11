277de121c961b8a5e04c8f69ec3d93140d5dd354
Drifter101 <93456343+Drifter101@users.noreply.github.com>
2022-08-11 14:54:04 +0200

    Create Weatherdata_webscrapper.py

---
+++ Weatherdata_webscrapper.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 09:58:30 2022
+
+@author: Nicol
+"""
+
+# Climate data scrapper 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+import pmdarima as pm
+import meteostat
+
+from datetime import datetime
+from meteostat import Point, Daily, Monthly, Normals, Hourly
+from meteostat import Stations
+from tqdm import tqdm_notebook
+import itertools
+from itertools import product
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Set time period
+start = datetime(1970, 1, 1)
+end = datetime(2021, 12, 30)
+
+#Use longitud and latitud for finding nearby stations
+stations = Stations()
+stations = stations.nearby(48.87340095547229, 2.357350985989415)
+stations = stations.fetch(5)
+print(stations)
+
+#Use the station code for downloading the data
+pt = Monthly("07149", start,end)
+pt = pt.normalize()
+pt = pt.aggregate("1Y")
+pt = pt.fetch()
+
+# Plot line chart including average, minimum and maximum temperature
+pt.plot(y=['tavg', 'tmin', 'tmax'])
+plt.show()
+
+
+# Other useful commands
+#Setting a month column and removing empty columns
+pt2 = pt
+pt2 = pt2.drop(pt2.columns[[4, 5, 6]], axis= 1)
+pt2 = pt2.dropna()
+pt2['month'] = pd.DatetimeIndex(pt2.index).month
+pt2.iloc[[0,-1]]
+
+#Check for missing values
+pt2[['prcp', 'tavg', 'tmax', 'tmin']].isna().sum()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
