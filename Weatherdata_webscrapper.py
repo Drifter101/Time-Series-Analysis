dda341735af60bc56e3ddd9a26d6ca426efd9788
Drifter101 <93456343+Drifter101@users.noreply.github.com>
2022-08-11 14:58:51 +0200

    Weatherdata webscrapper

--- Weatherdata_webscrapper.py
+++ Weatherdata_webscrapper.py
@@ -36,6 +36,18 @@ stations = stations.fetch(5)
 print(stations)
 
 #Use the station code for downloading the data
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+df = Monthly("07149", start,end)
+df = df.normalize()
+df = df.aggregate("1Y")
+df = df.fetch()
+
+# Plot line chart including average, minimum and maximum temperature
+df.plot(y=['tavg', 'tmin', 'tmax'])
+=======
+>>>>>>> Stashed changes
 pt = Monthly("07149", start,end)
 pt = pt.normalize()
 pt = pt.aggregate("1Y")
@@ -43,11 +55,28 @@ pt = pt.fetch()
 
 # Plot line chart including average, minimum and maximum temperature
 pt.plot(y=['tavg', 'tmin', 'tmax'])
+<<<<<<< Updated upstream
+=======
+>>>>>>> main
+>>>>>>> Stashed changes
 plt.show()
 
 
 # Other useful commands
 #Setting a month column and removing empty columns
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+df2 = df
+df2 = df2.drop(df2.columns[[4, 5, 6]], axis= 1)
+df2 = df2.dropna()
+df2['month'] = pd.DatetimeIndex(df2.index).month
+df2.iloc[[0,-1]]
+
+#Check for missing values
+df2[['prcp', 'tavg', 'tmax', 'tmin']].isna().sum()
+=======
+>>>>>>> Stashed changes
 pt2 = pt
 pt2 = pt2.drop(pt2.columns[[4, 5, 6]], axis= 1)
 pt2 = pt2.dropna()
@@ -56,6 +85,10 @@ pt2.iloc[[0,-1]]
 
 #Check for missing values
 pt2[['prcp', 'tavg', 'tmax', 'tmin']].isna().sum()
+<<<<<<< Updated upstream
+=======
+>>>>>>> main
+>>>>>>> Stashed changes
 
 
 
